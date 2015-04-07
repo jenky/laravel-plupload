4126d0423bc35c5f33251a1cc0f2dbb1ff14c485
Milano <jenky.w0w@gmail.com>
2015-04-07 16:15:10 +0700

    Revert "Revert "Upload plupload.php""
    This reverts commit ad701b549f949e71484b2bd17a5f299f082234a7.

--- config/plupload.php
+++ config/plupload.php
@@ -3,6 +3,17 @@
 return [
 	'chunk_path' => storage_path('plupload'),
 	
-	'flash_swf_url' => asset("assets/plupload/js/Moxie.swf"),
-	'silverlight_xap_url' => asset("assets/plupload/js/Moxie.xap"),
+	/*
+	|--------------------------------------------------------------------------
+	| Plupload global options
+	|--------------------------------------------------------------------------
+	|
+	| Set default global options for Plupload
+	|
+	| See https://github.com/moxiecode/plupload/wiki/Options
+	|
+	*/
+
+	'flash_swf_url' => public_path('assets/plupload/js/Moxie.swf'),
+	'silverlight_xap_url' => public_path('assets/plupload/js/Moxie.xap'),
 ];
