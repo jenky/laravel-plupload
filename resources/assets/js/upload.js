d7f0805e0f0cb0c6e95f8652b1a064428cf63399
Lynh <milano1411@gmail.com>
2016-04-05 15:29:44 +0700

    Tweak file

--- resources/assets/js/upload.js
+++ resources/assets/js/upload.js
@@ -1,120 +1,121 @@
 function createUploader(uploaderId)
 {
-	var $uploader = $('#uploader-' + uploaderId);
+    var $uploader = $('#uploader-' + uploaderId);
 
-	if (!$uploader || !$uploader.length) {
-		alert('Cannot find uploader');
-	}
+    if (!$uploader || !$uploader.length) {
+        alert('Cannot find uploader');
+    }
 
-	var $filelist = $uploader.find('.filelist'),
-		$uploaded = $uploader.find('.uploaded'),
-		$uploadAction = $uploader.find('.upload-actions'),
-		$uploadBtn = $('#' + $uploader.data('uploadbtn')) || false,
-		options = $uploader.data('options') || {},
-		autoStart = $uploader.data('autostart') || false,
-		deleteUrl = $uploader.data('deleteurl') || false,
-		deleteMethod = $uploader.data('deletemethod') || 'DELETE';
+    var $filelist = $uploader.find('.filelist'),
+        $uploaded = $uploader.find('.uploaded'),
+        $uploadAction = $uploader.find('.upload-actions'),
+        $uploadBtn = $('#' + $uploader.data('uploadbtn')) || false,
+        options = $uploader.data('options') || {},
+        autoStart = $uploader.data('autostart') || false,
+        deleteUrl = $uploader.data('deleteurl') || false,
+        deleteMethod = $uploader.data('deletemethod') || 'DELETE';
 
-	defaultOptions = {
-		init: {
-			PostInit: function(up) {   
-				if (!autoStart && $uploadBtn) {
-					$uploadBtn.click(function() {
-						up.start();
-						return false;
-					});                         
-				}
-			},
-	 
-			FilesAdded: function(up, files) {
-				$.each(files, function(i, file){
-					$filelist.append(
-						'<div id="' + file.id + '" class="alert alert-file">' +
-						'<div class="filename hide">' + file.name + ' (' + plupload.formatSize(file.size) + ')  <button type="button" class="close cancelUpload">&times;</button></div>' +
-						'<div class="progress progress-striped"><div class="progress-bar" style="width: 0;"></div></div></div>');
+    defaultOptions = {
+        init: {
+            PostInit: function(up) {
+                if (!autoStart && $uploadBtn) {
+                    $uploadBtn.click(function() {
+                        up.start();
+                        return false;
+                    });
+                }
+            },
 
-					$filelist.on('click', '#' + file.id + ' button.cancelUpload', function() {
-						var $this = $(this),
-							$file = $('#' + file.id),
-							deleteUrl = $this.data('deleteurl') || false,
-							id = $this.data('id') || false;
-						
-						if (deleteUrl) {
-							$.ajax({
-								dataType: 'json',
-								type: deleteMethod,
-								url: deleteUrl,
-								data: options.multipart_params,
-								success: function(result) {                                  
-									if (result.success) {
-										up.removeFile(file);
-										$('#' + file.id).remove();
-										$('#' + file.id + '-hidden').remove();
-										$uploadAction.show();
-									}
-									else {
-										$('#' + file.id).append('<span class="text-danger">' + result.message + '</span>');
-									}
-								}
-							});
-						}
-						else {
-							$uploadAction.show();     
-							$file.hide(); 
-							up.removeFile(file);                 
-						}
-					});
-				}); 
-				up.refresh(); // Reposition Flash/Silverlight
-				if (autoStart) {
-					$uploadAction.hide();
-					up.start();
-				}
-			},
-	 
-			UploadProgress: function(up, file) {   
-				$uploadAction.hide();             
-				$('#' + file.id + ' .progress').addClass('active');
-				$('#' + file.id + ' button.cancelUpload').hide();
-				$('#' + file.id + ' .progress .progress-bar').animate({width: file.percent + '%'}, 100, 'linear');
-			},
-	 
-			Error: function(up, err) {
-				$filelist.append('<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert">&times;</button>' +
-					'Error: ' + err.code + ', Message: ' + err.message +
-						(err.file ? ', File: ' + err.file.name : '') +
-						"</div>"
-				);
-				up.refresh(); // Reposition Flash/Silverlight
-			},
+            FilesAdded: function(up, files) {
+                $.each(files, function(i, file){
+                    $filelist.append(
+                        '<div id="' + file.id + '" class="alert alert-file">' +
+                        '<div class="filename hide">' + file.name + ' (' + plupload.formatSize(file.size) + ')  <button type="button" class="close cancelUpload">&times;</button></div>' +
+                        '<div class="progress progress-striped"><div class="progress-bar" style="width: 0;"></div></div></div>');
 
-			FileUploaded: function(up, file, info) {
-				var response = JSON.parse(info.response);
+                    $filelist.on('click', '#' + file.id + ' button.cancelUpload', function() {
+                        var $this = $(this),
+                            $file = $('#' + file.id),
+                            deleteUrl = $this.data('deleteurl') || false,
+                            id = $this.data('id') || false;
 
-				$('#' + file.id + ' .progress .progress-bar').animate({width: '100%'}, 100,  'linear');
-				$('#' + file.id + ' .progress').removeClass('progress-striped').removeClass('active').fadeOut();
-				$('#' + file.id + ' .filename').removeClass('hide').show();                
-				$('#' + file.id + ' button.cancelUpload').show();                
-				
-				if (response.result.id) {
-					$('#' + file.id + ' button.cancelUpload').attr('data-id', response.result.id);
-					$('<input type="hidden" name="' + uploaderId + '_files[]" value="' + response.result.id + '" id="' + file.id + '-hidden">').appendTo($uploader);
-				}
+                        if (deleteUrl) {
+                            $.ajax({
+                                dataType: 'json',
+                                type: deleteMethod,
+                                url: deleteUrl,
+                                data: options.multipart_params,
+                                success: function(result) {
+                                    if (result.success) {
+                                        up.removeFile(file);
+                                        $file.remove();
+                                        $('#' + file.id + '-hidden').remove();
+                                        $uploadAction.show();
+                                    }
+                                    else {
+                                        $('#' + file.id).append('<span class="text-danger">' + result.message + '</span>');
+                                    }
+                                }
+                            });
+                        }
+                        else {
+                            $uploadAction.show();
+                            $file.remove();
+                            $('#' + file.id + '-hidden').remove();
+                            up.removeFile(file);
+                        }
+                    });
+                });
+                up.refresh(); // Reposition Flash/Silverlight
+                if (autoStart) {
+                    $uploadAction.hide();
+                    up.start();
+                }
+            },
 
-				if (response.result.deleteUrl) {
-					$('#' + file.id + ' button.cancelUpload').attr('data-deleteurl', response.result.deleteUrl);
-				}
+            UploadProgress: function(up, file) {
+                $uploadAction.hide();
+                $('#' + file.id + ' .progress').addClass('active');
+                $('#' + file.id + ' button.cancelUpload').hide();
+                $('#' + file.id + ' .progress .progress-bar').animate({width: file.percent + '%'}, 100, 'linear');
+            },
 
-				if (response.result.url) {
-					$('#' + file.id).append('<img src="' + response.result.url + '" class="img-responsive img-thumbnail" />');
-				}
+            Error: function(up, err) {
+                $filelist.append('<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert">&times;</button>' +
+                    'Error: ' + err.code + ', Message: ' + err.message +
+                        (err.file ? ', File: ' + err.file.name : '') +
+                        "</div>"
+                );
+                up.refresh(); // Reposition Flash/Silverlight
+            },
 
-			}
-		}
-	};
+            FileUploaded: function(up, file, info) {
+                var response = JSON.parse(info.response);
 
-	$.extend(options, defaultOptions);
+                $('#' + file.id + ' .progress .progress-bar').animate({width: '100%'}, 100,  'linear');
+                $('#' + file.id + ' .progress').removeClass('progress-striped').removeClass('active').fadeOut();
+                $('#' + file.id + ' .filename').removeClass('hide').show();
+                $('#' + file.id + ' button.cancelUpload').show();
 
-	var uploader = new plupload.Uploader(options);
-	uploader.init();
+                if (response.result.id) {
+                    $('#' + file.id + ' button.cancelUpload').attr('data-id', response.result.id);
+                    $('<input type="hidden" name="' + uploaderId + '_files[]" value="' + response.result.id + '" id="' + file.id + '-hidden">').appendTo($uploader);
+                }
+
+                if (response.result.deleteUrl) {
+                    $('#' + file.id + ' button.cancelUpload').attr('data-deleteurl', response.result.deleteUrl);
+                }
+
+                if (response.result.url) {
+                    $('#' + file.id).append('<img src="' + response.result.url + '" class="img-responsive img-thumbnail" />');
+                }
+
+            }
+        }
+    };
+
+    $.extend(options, defaultOptions);
+
+    var uploader = new plupload.Uploader(options);
+    uploader.init();
 }
